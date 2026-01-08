c6bc1ae3cc882c0843c0b8da7c71f3f328810f59
Sahil khan <sahilkhan987152@gmail.com>
2026-01-08 10:09:28 +0530

    mergeadded

--- Day 3/Sorting.cpp
+++ Day 3/Sorting.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 //BUBBLE SORT
@@ -26,11 +27,54 @@ void selection(int arr[] , int n){
 }
 }
 
+void mergeit(int arr[], int start , int end , int mid){
+   vector<int>temp;
+   int i = start ;
+   int j = mid+1;
+
+   while(i <= mid && j<= end){
+   if(arr[i]<arr[j]){
+    temp.push_back(arr[i]);
+    i++;
+   } else{
+    temp.push_back(arr[j]);
+    j++;
+   }
+
+}
+   while(i<= mid){
+    temp.push_back(arr[i]);
+    i++;
+   }
+
+   while(j<= end){
+    temp.push_back(arr[j]);
+    j++;
+   }
+    int k = 0;
+   for(int p = start ; p<=end;  p++){
+    arr[p] = temp[k];
+    k++;
+   }
+}
+void merge_sort(int arr[] , int start , int end){
+
+//base condition 
+if(start >= end) return ;
+
+int mid = (start + end)/2;
+
+merge_sort(arr , start , mid);
+merge_sort(arr , mid+1 , end);
+mergeit(arr, start , end , mid);
+
+}
 int main(){
 int arr[] ={1,5,2,4,9,2};
 int n = 6;
 
 bubble(arr , n);
 selection(arr , n);
+
     return 0;
 }
